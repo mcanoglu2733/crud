b886db0d55b15de2e742be5b2299841c9e383c1e
mcanoglu2733 <74180907+mcanoglu2733@users.noreply.github.com>
2023-02-20 10:30:06 +0100

    user migrate
    database for users has been migrated

--- crud/database/migrations/2014_10_12_000000_create_users_table.php
+++ crud/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('user_name')->unique();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
